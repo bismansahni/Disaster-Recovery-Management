4db671d2a73ff08acbc9dc813550df32734fd8f6
bismansahni <bismansahni@outlook.com>
2024-09-09 18:04:03 -0700

    updated the model to latest

--- model/model.py
+++ model/model.py
@@ -1,40 +1,3 @@
-# import pandas as pd
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-# import joblib
-
-# # Load the preprocessed and split data
-# file_path = '../dataset/storms-preprocessed.csv'
-# storms_data = pd.read_csv(file_path)
-
-# # Define the features (X) and target (y)
-# X_train = storms_data[['lat', 'long', 'wind', 'pressure']].iloc[:int(len(storms_data)*0.8)]
-# X_test = storms_data[['lat', 'long', 'wind', 'pressure']].iloc[int(len(storms_data)*0.8):]
-# y_train = storms_data['status_next_24h'].iloc[:int(len(storms_data)*0.8)]
-# y_test = storms_data['status_next_24h'].iloc[int(len(storms_data)*0.8):]
-
-# # Initialize the Logistic Regression model
-# model = LogisticRegression(random_state=42)
-
-# # Train the model on the training data
-# model.fit(X_train, y_train)
-
-# # Predict on the test data
-# y_pred = model.predict(X_test)
-
-# # Evaluate the model
-# accuracy = accuracy_score(y_test, y_pred)
-# report = classification_report(y_test, y_pred)
-# conf_matrix = confusion_matrix(y_test, y_pred)
-
-# # Print the results
-# print("Model Accuracy:", accuracy)
-# print("\nClassification Report:\n", report)
-# print("\nConfusion Matrix:\n", conf_matrix)
-
-# # Save the trained model to a file
-# joblib.dump(model, 'hurricane_prediction_model.pkl')
-
 
 
 
@@ -75,4 +38,4 @@ print("\nClassification Report:\n", report)
 print("\nConfusion Matrix:\n", conf_matrix)
 
 # Save the trained model to a file
-joblib.dump(model, 'hurricane_prediction_gb_model.pkl')
+joblib.dump(model, 'hurricane_prediction_latest_model.pkl')
